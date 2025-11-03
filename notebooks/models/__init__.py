719a5a44961d156e35ac7717b9dbb1f93eed601e
Amir Zarandi <90559838+amirzarandi@users.noreply.github.com>
2025-11-03 13:36:29 -0500

    Stock trend classification with portfolio benchmark

--- notebooks/models/__init__.py
+++ notebooks/models/__init__.py
@@ -3,4 +3,5 @@ from .evaluate import measure_accuracy, get_regression_error, plot_regression
 from .gat import GAT
 from .gcn import GCN
 from .tgcncell import TGCNCell
+from .tgcn import TGCN
 from .train import train
